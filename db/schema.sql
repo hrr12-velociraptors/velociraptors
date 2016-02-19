bf6b70ddfd99c992103987a24fe5b75b13995158
Kelly von Borstel <kellyvonborstel@gmail.com>
2016-02-18 19:27:33 -0800

    Updated database

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,8 @@
-DROP DATABASE learnItNowdb;
 Create DATABASE learnItNowdb;
 
 USE learnItNowdb;
 
-CREATE TABLE tutors (
+CREATE TABLE users (
   userId INT PRIMARY KEY AUTO_INCREMENT,
   username VARCHAR(15) NOT NULL,
   email VARCHAR(24) NOT NULL,
@@ -12,13 +11,11 @@ CREATE TABLE tutors (
 
 CREATE TABLE sessions (
   sessionId INT PRIMARY KEY AUTO_INCREMENT,
-  userId INT FOREIGN KEY, 
+  userId INT, 
   topic VARCHAR(20) NOT NULL,
   description VARCHAR(100) NOT NULL,
   startTime DATETIME NOT NULL,
   link TEXT NOT NULL,
-  status BOOLEAN NOT NULL
+  status BOOLEAN NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(userId)
 );
-
-
--- INSERT tutors (userId, username, email, password) VALUES ('12345', 'tinymonster', 'password123');

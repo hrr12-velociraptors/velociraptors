be266d53df0db3b3d5266ef7b5299f85e09a6b5e
pedrotp <pedro@pedrotp.com>
2016-02-23 17:39:37 -0500

    added filter by date

--- client/createSession/sessionController.js
+++ client/createSession/sessionController.js
@@ -8,17 +8,25 @@ myApp.controller('SessionController', function($scope, Session) {
     });
   };
   $scope.getSessions();
-
+  $scope.sessionFilter(session) {
+    var today = new Date();
+    var sessionTime = new Date(session.startTime.replace(/-/g,"/"));
+    if ( $scope.filterType === 'all') {
+      return true;
+    } else if ($scope.filterType === 'day') {
+      return sessionTime.getDay() === today.getDay() && sessionTime.getMonth() === today.getMonth() && sessionTime.getFullYear() === today.getFullYear();
+    } else if ($scope.filterType === 'week') {
+      return true;
+    }
+  };
   $scope.updateStatus = function(index){
     var session = $scope.sessions[index];
     var updateInfo = {id: session.id, status: true };
-
     Session.updateStatus(updateInfo).then(function(updatedSession){
       $scope.getSessions();
-    })
-  }
+    });
+  };
 })
-
 .controller('CreateSessionController', function($scope, Session, Auth, $window) {
   $scope.session = {};
   

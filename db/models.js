b37cf8b083b0c19c6d379c4ddd06f5da5daf7cd6
rswerve <rswerve@gmail.com>
2016-02-23 00:11:14 -0600

    changed database settings

--- db/models.js
+++ db/models.js
@@ -3,7 +3,18 @@
 //         //
 
 var Sequelize = require('sequelize');
-var orm = new Sequelize('learnItNowdb', 'root', '');
+var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+var orm = new Sequelize(match[5], match[1], match[2], {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging: false,
+    dialectOptions: {
+        ssl: true
+    }
+});
+//var orm = new Sequelize('learnItNowdb', 'root', '');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 

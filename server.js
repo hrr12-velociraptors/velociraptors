130801c3b4623ae457a67b6b6f19a7f9c658556c
rswerve <rswerve@gmail.com>
2016-02-22 22:41:21 -0600

    messing around with middleware

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-require('./config/middleware.js')(app,express);
+//require('./config/middleware.js')(app,express);
 
 app.listen(port, function() {
   console.log('Velociraptors server listening on port ' + port);

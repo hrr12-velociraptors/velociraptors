4fdb30bb257a28d97bad764d5c97c8c5d6c08f08
rswerve <rswerve@gmail.com>
2016-02-26 16:48:20 -0600

    code cleanup for front end

--- client/auth/signupController.js
+++ client/auth/signupController.js
@@ -1,9 +1,9 @@
-
+// invokes function from mainFactory.js to make a signup request
 myApp.controller('SignupController', function($scope, $window, Auth) {
   $scope.user = {};
   $scope.signup = function(user) {
     Auth.signup(user).then(function(data){
-    	console.log(data);
+      // redirect
     	$window.location.href = '/#/signin';
     });
   };
